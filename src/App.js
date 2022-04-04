e49a3e1a2b9873c91ae062d4e38b1df43785e25d
Wahidul30 <wahidul505@gmail.com>
2022-04-05 03:24:29 +0600

    added a background image with fixed positioning

--- src/App.js
+++ src/App.js
@@ -4,10 +4,19 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './Components/Home/Home';
 import Volunteers from './Components/Volunteers/Volunteers';
 import NotFound from './Components/NotFound/NotFound';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [yAxis, setYAxis] = useState(0);
+  const handleScroll = () => setYAxis(window.pageYOffset);
+  useEffect(()=>{
+    window.addEventListener('scroll',handleScroll);
+    return ()=> window.removeEventListener('scroll', handleScroll);
+  },[])
+  console.log(yAxis);
   return (
-    <div className="App">
+    <div>
+    <div className='App'>
      <Header></Header>
      <Routes>
        <Route path='/' element={<Home />}></Route>
@@ -15,6 +24,10 @@ function App() {
        <Route path='/volunteers' element={<Volunteers />}></Route>
        <Route path='*' element={<NotFound />}></Route>
      </Routes>
+    </div>
+    <div className='bg'>
+
+    </div>
     </div>
   );
 }
